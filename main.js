da213acd66b4d626f9ecea18216441a51dde9cf7
Aryan Bamane <aryanbamane2@gmail.com>
2023-10-02 18:25:29 +0530

    Updating styles

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ import './components/footer.css'
 import './components/header.css'
 import './components/intro.css'
 import './components/projects.css'
-import './components/responsive.css'
 import './components/skills.css'
 import './components/style.css'
 
