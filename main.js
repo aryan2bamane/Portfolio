0ab1f5a2f10da48559e9305f0882ca23751e8c75
Aryan Bamane <aryanbamane2@gmail.com>
2023-10-02 22:10:11 +0530

    Updating styles

--- main.js
+++ main.js
@@ -8,3 +8,10 @@ import './components/skills.css'
 import './components/style.css'
 
 
+let hamburger = document.querySelector(".hamburger");
+let header = document.querySelector("header");
+let nav = document.querySelector("nav");
+hamburger.addEventListener("click", () => {
+    header.classList.toggle("expanded");
+    nav.classList.toggle("active");
+})
